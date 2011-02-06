6cd44bb0c5eb1279073ebcd5935909031a434b4a
Alex Michael <am607@imperial.ac.uk>
2011-02-06 15:49:08 +0000

    added --trace to build.ors

--- script/build/ors/build_ors.sh
+++ script/build/ors/build_ors.sh
@@ -14,7 +14,7 @@ ORS_MAIN_DIR=~/Desktop/Dev/IndividualProject/ors
 # cd into the main dir and invoke buildr.
 cd $ORS_MAIN_DIR
 echo "Building ORS with buildr.."
-buildr release
+buildr release --trace
 retCode=$?
 if [[ $retCode -ne 0 ]]
 then
